e69bf0a2cdb49135f4df761c9dd984583ee5e8ba
SukhrobWeb <83048281+astonaqulovsukhrob@users.noreply.github.com>
2025-11-28 18:26:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
-app.use(express.json());
-app.use(bodyParser.json());
-
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 // Eski users API
 app.use("/people", usersRoutes);
 
