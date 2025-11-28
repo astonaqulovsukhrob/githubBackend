8cda10ba166b65f739509ef8d3f8830ad8bdc0bc
SukhrobWeb <83048281+astonaqulovsukhrob@users.noreply.github.com>
2025-11-28 15:34:35 +0500

    Add new fields to moduleB schema

--- models/moduleB.js
+++ models/moduleB.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema({
   name: String,
   title: String,
-  var: String,
+  moduleA: String,
+  moduleB: String,
+  moduleC: String,
+  moduleD: String,
   img: String,
 });
 
